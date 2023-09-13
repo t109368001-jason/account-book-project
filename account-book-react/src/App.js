93b903e4ea297a74a211e34822983cb50e0fda34
jason-xiao <t105360046@ntut.org.tw>
2023-09-13 14:50:02 +0800

    <fix> fix redirect bug

--- account-book-react/src/App.js
+++ account-book-react/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import { Navigate, Route, Routes } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
@@ -11,22 +11,29 @@ import ABOverview from "./pages/ABOverview";
 
 function App() {
   const dispatch = useDispatch();
+  const [init, setInit] = useState(false);
 
   useEffect(() => {
-    dispatch(getUser());
+    dispatch(getUser()).then(() => {
+      setInit(true);
+    });
   }, []);
+
+  if (!init) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
       <Header />
       <Routes>
-        <Route index element={<IndexPage />} />
         <Route path={"login"} element={<LoginPage />} />
         <Route path={"account-book"} element={<AccountBookPage />}>
-          <Route index element={<ABOverview />} />
           <Route path={"record"} element={<RecordPage />} />
+          <Route index element={<ABOverview />} />
           <Route path={"*"} element={<Navigate to="" />} />
         </Route>
+        <Route index element={<IndexPage />} />
         <Route path={"*"} element={<Navigate to="" />} />
       </Routes>
     </>
