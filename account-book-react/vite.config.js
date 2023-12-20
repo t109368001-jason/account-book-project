4d84db6764533c4f56bd49aa41331c4f77ce897e
jason-xiao <t105360046@ntut.org.tw>
2023-12-20 15:33:52 +0800

    <3rd> remove unused deps

--- account-book-react/vite.config.js
+++ account-book-react/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
+import viteJsconfigPaths from "vite-jsconfig-paths";
 
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: "",
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), viteJsconfigPaths()],
   server: {
     // this ensures that the browser opens upon server start
     open: true,
